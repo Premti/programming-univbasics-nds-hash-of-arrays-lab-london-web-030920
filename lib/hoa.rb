c05bd3a4b6b06e28d7db1107f6da86d0357bf7ec
premti <premti@gmail.com>
2020-02-25 16:24:52 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -9,5 +9,5 @@ def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
   show[:chipmunks]<<(name)
-  return :chipmunks
+  return show
 end
